4e361e74d9a173a9335c98f278ac908c53d69aed
Haru Im <haru@Haruui-MacBookAir.local>
2023-08-05 19:40:54 +0900

    chore: delete duplicate eslintrc rule + tsconfig strict

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,17 +9,10 @@ module.exports = {
   },
   ignorePatterns: ['node_modules', 'dist'],
   extends: ['plugin:react/recommended'],
-  plugins: [
-    '@typescript-eslint',
-    'import',
-    'unused-imports',
-    'react-hooks',
-    'unused-imports',
-  ],
+  plugins: ['@typescript-eslint', 'import', 'unused-imports', 'react-hooks'],
   rules: {
     'unused-imports/no-unused-imports-ts': 'error',
     'react/react-in-jsx-scope': 'off',
-    'unused-imports/no-unused-imports-ts': 'error',
   },
   settings: {
     react: {
