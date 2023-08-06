07ff36f26bd444ed0970ec46eb1ffbcfd5663350
junho01052 <junho010522@gmail.com>
2023-08-06 21:28:44 +0900

    chore: add a introduction page
    change a new default path

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Introduction } from '../pages/Introduction/Introduction';
 
 export const RouterProvider = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
+        <Route path="/" element={<Introduction />} />
       </Routes>
     </BrowserRouter>
   );
