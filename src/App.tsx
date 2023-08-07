7d1ee2467084a30335e66eaa746e54f24c6079e8
Haru Im <haru@Haruui-MacBookAir.local>
2023-08-08 01:23:55 +0900

    feat: add theme provider with color definitions

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { FC } from 'react';
 import { ReactQueryProvider, RecoilProvider } from './providers';
 import { RouterProvider } from './router';
+import { ThemeProvider } from 'styled-components';
+import { Theme } from './styles';
 
 export const App: FC = () => {
   return (
     <>
       <RecoilProvider>
         <ReactQueryProvider>
-          <RouterProvider />
+          <ThemeProvider theme={Theme}>
+            <RouterProvider />
+          </ThemeProvider>
         </ReactQueryProvider>
       </RecoilProvider>
     </>
---
+++ src/styles/theme.ts
@@ -0,0 +1,19 @@
+interface ThemeProps {
+  colors: {
+    pink: string;
+    gray: string;
+    blue: string;
+    white: string;
+    black: string;
+  };
+}
+
+export const Theme: ThemeProps = {
+  colors: {
+    pink: '#FF005E',
+    gray: '#D9D9D9',
+    blue: '#5782FA',
+    white: '#FEFEFE',
+    black: '#14171F',
+  },
+};
