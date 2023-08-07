e6ad514b8fbd06d59bc3fff551d69c98a6cabc36
junho01052 <junho010522@gmail.com>
2023-08-07 22:33:24 +0900

    refactor: refactoring using map method

--- src/pages/Introduction/views/TextSlideView/TextSlideView.tsx
+++ src/pages/Introduction/views/TextSlideView/TextSlideView.tsx
@@ -2,12 +2,14 @@ import { FlowText, FlowWrap } from './style';
 
 export const TextSlideView = () => {
   const text = `행복한 동네를 위한 대화의 장소 `;
+
+  const texts = new Array(4).fill(text);
+
   return (
     <FlowText>
-      <FlowWrap>{text}</FlowWrap>
-      <FlowWrap>{text}</FlowWrap>
-      <FlowWrap>{text}</FlowWrap>
-      <FlowWrap>{text}</FlowWrap>
+      {texts.map((item, index) => (
+        <FlowWrap key={index}>{item}</FlowWrap>
+      ))}
     </FlowText>
   );
 };
