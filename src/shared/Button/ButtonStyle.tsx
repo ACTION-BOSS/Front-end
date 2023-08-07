64ef609545f5ae7324c4fa186ce18f5b3d891f6e
Haru Im <haru@Haruui-MacBookAir.local>
2023-08-08 01:35:35 +0900

    feat: add new button theme props + separate types, theme, styled-components

--- src/shared/Button/ButtonStyle.tsx
+++ src/shared/Button/ButtonStyle.tsx
@@ -1,71 +1,8 @@
-import { ButtonHTMLAttributes } from 'react';
-import { styled, css, DefaultTheme } from 'styled-components';
+import { styled, css } from 'styled-components';
+import { ButtonStyleProps } from './type';
+import { $buttonTheme } from './buttonTheme';
 
-interface IButtonStyleProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  $buttonTheme: keyof typeof $buttonTheme;
-  theme: Mytheme;
-  size: string;
-  $bold?: boolean;
-}
-
-interface Mytheme extends DefaultTheme {
-  filled: {
-    backgroundColor: string;
-    color: string;
-    border: string;
-    hover: {
-      backgroundColor: string;
-    };
-  };
-
-  empty: {
-    backgroundColor: string;
-    color: string;
-    border: string;
-    hover: {
-      backgroundColor: string;
-    };
-  };
-
-  gray: {
-    backgroundColor: string;
-    color: string;
-    border: string;
-    hover: {
-      backgroundColor: string;
-    };
-  };
-}
-
-const $buttonTheme = {
-  filled: {
-    backgroundColor: '#FF005E',
-    color: '#fff',
-    border: '1px solid transparent',
-    hover: {
-      backgroundColor: '#ff478a',
-    },
-  },
-  empty: {
-    backgroundColor: '#fff',
-    color: 'black',
-    border: '1px solid rgba(0, 0, 0, 0.25)',
-    hover: {
-      backgroundColor: '#ececec',
-    },
-  },
-
-  gray: {
-    backgroundColor: '#D9D9D9',
-    color: '#A2A2A2',
-    border: '1px solid transparent',
-    hover: {
-      backgroundColor: '#dfdfdf',
-    },
-  },
-};
-
-export const ButtonStyle = styled.button<IButtonStyleProps>`
+export const ButtonStyle = styled.button<ButtonStyleProps>`
   display: flex;
   flex: 1;
   text-decoration: none;
@@ -76,20 +13,18 @@ export const ButtonStyle = styled.button<IButtonStyleProps>`
     border-color 0.1s;
   cursor: pointer;
   align-items: center;
-  padding-top: ${(props) => (props.size === 'large' ? '22.25px' : '15.75px')};
-  padding-bottom: ${(props) =>
-    props.size === 'large' ? '22.25px' : '15.75px'};
+  padding-top: ${(props) => (props.size === 'large' ? '16px' : '12px')};
+  padding-bottom: ${(props) => (props.size === 'large' ? '16px' : '12px')};
   padding-left: 16px;
   padding-right: 16px;
   box-sizing: border-box;
-  border: 1px solid transparent;
   font: inherit;
   border-radius: 11px;
   background-color: ${(props) =>
     $buttonTheme[props.$buttonTheme].backgroundColor};
   color: ${(props) => $buttonTheme[props.$buttonTheme].color};
   font-size: ${(props) => (props.size === 'large' ? '18px' : '14px')};
-  font-weight: ${(props) => (props.$bold ? '600' : '400')};
+  font-weight: ${(props) => (props.$bold ? '700' : '400')};
   border: ${(props) => $buttonTheme[props.$buttonTheme].border};
   ${(props) => css`
     &:hover,
---
+++ src/shared/Button/type.ts
@@ -0,0 +1,67 @@
+import { ButtonHTMLAttributes } from 'react';
+import { DefaultTheme } from 'styled-components';
+import { $buttonTheme } from './buttonTheme';
+
+export interface ButtonStyleProps
+  extends ButtonHTMLAttributes<HTMLButtonElement> {
+  $buttonTheme: keyof typeof $buttonTheme;
+  theme: Mytheme;
+  size: string;
+  $bold?: boolean;
+}
+
+export interface Mytheme extends DefaultTheme {
+  pink: {
+    backgroundColor: string;
+    color: string;
+    border: string;
+    hover: {
+      backgroundColor: string;
+    };
+  };
+
+  emptyBlue: {
+    backgroundColor: string;
+    color: string;
+    border: string;
+    hover: {
+      backgroundColor: string;
+    };
+  };
+
+  gray: {
+    backgroundColor: string;
+    color: string;
+    border: string;
+    hover: {
+      backgroundColor: string;
+    };
+  };
+
+  blue: {
+    backgroundColor: string;
+    color: string;
+    border: string;
+    hover: {
+      backgroundColor: string;
+    };
+  };
+
+  emptyGray: {
+    backgroundColor: string;
+    color: string;
+    border: string;
+    hover: {
+      backgroundColor: string;
+    };
+  };
+
+  emptyPink: {
+    backgroundColor: string;
+    color: string;
+    border: string;
+    hover: {
+      backgroundColor: string;
+    };
+  };
+}
