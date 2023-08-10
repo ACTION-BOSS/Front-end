fc180fd0e0d48b2b2f8d9edfe7631237d6ab2f4c
HyeonE <mag30102000@gmail.com>
2023-08-11 01:34:44 +0900

    design: update main page sidebar ui

--- src/pages/MainPage/components/mainPosts/MainPosts.tsx
+++ src/pages/MainPage/components/mainPosts/MainPosts.tsx
@@ -1,25 +1,45 @@
 import React, { useState } from 'react';
 import * as s from './MainPostsStyle';
 import { MainPost } from '../mainPost/MainPost';
+import {
+  CheckIcon,
+  CheckXIcon,
+  Toggle2Icon,
+  ToggleIcon,
+} from '../../../../assets';
 
 export const MainPosts = () => {
-  const [option, setOption] = useState('최신순');
-  const [toggle, setToggle] = useState(true);
-  const OPTIONS = [
-    { value: 'latest', name: '최신순' },
-    { value: 'like', name: '좋아요순' },
-    { value: 'done', name: '완료순' },
-  ];
+  const [currentOption, setCurrentOption] = useState('최신순');
+  const [isToggle, setIsToggle] = useState(true);
+  const OPTIONS = ['최신순', '불편순', '해결순'];
+
+  const onClickToggleHandler = () => {
+    setIsToggle(!isToggle);
+  };
+
+  const onClickOptionHandler = (option: string) => {
+    setCurrentOption(option);
+    setIsToggle(!isToggle);
+  };
+
   return (
     <s.MainPostsConatiner>
       <s.MainPostHeader>
         <div>우리동네 민원들</div>
         <div>
           <div>
-            <s.OptionButton>
-              <div>{option}</div>
+            <s.OptionButton onClick={onClickToggleHandler}>
+              <div>{currentOption}</div>
+              {isToggle ? <Toggle2Icon size={16} /> : <ToggleIcon size={16} />}
             </s.OptionButton>
-            <div></div>
+            <s.OptionList active={isToggle.toString()}>
+              {OPTIONS.map((item, index) => (
+                <li key={index} onClick={() => onClickOptionHandler(item)}>
+                  {item}
+                  {currentOption === item ? <CheckIcon /> : <CheckXIcon />}
+                </li>
+              ))}
+            </s.OptionList>
           </div>
         </div>
       </s.MainPostHeader>
@@ -27,7 +47,7 @@ export const MainPosts = () => {
         {Array(5)
           .fill(0)
           .map((item, index) => (
-            <MainPost key={index} />
+            <MainPost key={index} currentOption={currentOption} />
           ))}
       </s.MainPosts>
     </s.MainPostsConatiner>
