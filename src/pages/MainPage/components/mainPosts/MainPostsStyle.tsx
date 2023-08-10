80fec944be9d6dbc35fa2bd3318d6d98a8268bfc
HyeonE <mag30102000@gmail.com>
2023-08-11 01:34:44 +0900

    design: update main page sidebar ui

--- src/pages/MainPage/components/mainPosts/MainPostsStyle.tsx
+++ src/pages/MainPage/components/mainPosts/MainPostsStyle.tsx
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
+interface OptionProps {
+  active: string;
+}
+
 export const MainPostsConatiner = styled.div`
   width: 330px;
   box-sizing: border-box;
@@ -18,20 +22,43 @@ export const MainPostHeader = styled.div`
 export const OptionButton = styled.div`
   width: 70px;
   height: 30px;
+  border-radius: 10px;
+  border: 1px solid ${(props) => props.theme.colors.gray4};
   font-size: ${(props) => props.theme.fontSizes.body3};
   color: ${(props) => props.theme.colors.gray3};
   display: flex;
   align-items: center;
   justify-content: center;
-  img {
-    width: 16px;
-    height: 16px;
-    color: red;
+  gap: 4px;
+  box-shadow: 0px 0px 2px 0px ${(props) => props.theme.colors.gray8};
+  cursor: pointer;
+`;
+export const OptionList = styled.ul<OptionProps>`
+  position: fixed;
+  right: 30px;
+  background-color: white;
+  display: ${(props) => (props.active === 'true' ? 'flex' : 'none')};
+  flex-direction: column;
+  align-items: center;
+  width: 95px;
+  padding: 8px;
+  margin: 8px 0;
+  gap: 14px;
+  box-sizing: border-box;
+  border-radius: 10px;
+  box-shadow: 0px 2px 5px 0px ${(props) => props.theme.colors.gray8};
+  font-size: ${(props) => props.theme.fontSizes.body3};
+  z-index: 999;
+  li {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    cursor: pointer;
   }
 `;
 export const MainPosts = styled.div`
   padding: 0 45px;
-  height: calc(100vh - 145px);
+  height: calc(100vh - 151px);
   overflow-y: auto;
   &::-webkit-scrollbar {
     width: 10px;
