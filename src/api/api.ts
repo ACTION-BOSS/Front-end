591520c92592dc46e734b30ffaa6037ee922508a
junho01052 <junho010522@gmail.com>
2023-08-17 04:37:50 +0900

    chore: modify baseurl and change index.html script for deploy

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { Coordinates } from '../pages/MainPage/type';
 
 export const api = axios.create({
-  baseURL: '/api',
+  baseURL: `${process.env.REACT_APP_API_URI}/api`,
   headers: {
     'Content-Type': 'application/json',
   },
