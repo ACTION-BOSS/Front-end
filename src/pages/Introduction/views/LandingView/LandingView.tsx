15d9772f2ba3b5e63f8da3912a4846f933035084
junho01052 <junho010522@gmail.com>
2023-08-17 18:43:11 +0900

    chore: modify text for deploy test

--- src/pages/Introduction/views/LandingView/LandingView.tsx
+++ src/pages/Introduction/views/LandingView/LandingView.tsx
@@ -20,7 +20,7 @@ export const LandingView = () => {
   return (
     <StLandingView>
       <StLogoFlag src={LOGO_FLAG} alt="flag" />
-      <StBIName>행동대장</StBIName>
+      <StBIName>행동머장</StBIName>
       <StSlogan>행복한 동네를 위한 대화의 장소</StSlogan>
       <StText>당신의 행동으로 채우는 우리 동네, 우리 지도</StText>
       <StText className="wide">어쩌면 우리가 더 안전하고 행복해질 지도</StText>
