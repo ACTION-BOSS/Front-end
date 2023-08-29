12f7817b4c09ca89e920f3859eb056b2fdf4bb94
Haru-Im <chalk323@naver.com>
2023-08-29 17:42:56 +0900

    design: make content view responsive

--- src/pages/DetailPage/views/Content/ContentView.tsx
+++ src/pages/DetailPage/views/Content/ContentView.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import {
   StWrapper,
   StTitleWrapper,
@@ -8,13 +8,27 @@ import {
   StContentWord,
   StHowManyWrapper,
   StHowManyLetters,
+  StyledKebob,
+  StKebobIconWrapper,
+  StModifyButtonWrapper,
+  StyledPencilIcon,
+  StRowDiv,
+  StRowText,
 } from './ContentViewStyle';
+import { Theme } from '../../../../styles';
+import { BinIcon } from '../../../../assets';
+import { useWindowSize } from 'rooks';
+import { useDeleteData } from '../../container';
+import { EModalType, useModal } from '../../../../providers';
+import { useNavigate } from 'react-router';
 
 type ContentViewProps = {
   title: string;
   content: string;
   createdAt: string;
   nickname: string;
+  postId?: string;
+  owner: boolean;
 };
 
 export const ContentView: FC<ContentViewProps> = ({
@@ -22,11 +36,61 @@ export const ContentView: FC<ContentViewProps> = ({
   content,
   createdAt,
   nickname,
+  postId,
+  owner,
 }) => {
+  const [isModifyButtonOpened, setIsModifyButtonOpened] =
+    useState<boolean>(false);
+  const { deleteData } = useDeleteData();
+  const { openModal } = useModal();
+  const navigate = useNavigate();
+
+  const handleClickKebobIcon = () => {
+    setIsModifyButtonOpened((prev) => !prev);
+  };
+
+  const handleCickDeleteButton = () => {
+    deleteData();
+  };
+
+  const { innerWidth } = useWindowSize();
+  const isMobileView = innerWidth! < 576;
+
   return (
     <StWrapper>
       <StTitleWrapper>
-        <StTitle>{title}</StTitle>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <StTitle>{title}</StTitle>
+          {owner && isMobileView && (
+            <StKebobIconWrapper onClick={handleClickKebobIcon}>
+              <StyledKebob />
+              {isModifyButtonOpened && (
+                <StModifyButtonWrapper>
+                  <StRowDiv onClick={() => navigate(`/edit/${postId}`)}>
+                    <StRowText color={Theme.colors.gray7}>수정</StRowText>
+                    <StyledPencilIcon />
+                  </StRowDiv>
+                  <StRowDiv
+                    onClick={() =>
+                      openModal(EModalType.POP_UP, {
+                        title: '작성한 게시물을 삭제할까요?',
+                        cancelButton: true,
+                        functionButton: {
+                          theme: 'pink',
+                          label: '삭제',
+                          onClick: handleCickDeleteButton,
+                        },
+                      })
+                    }
+                  >
+                    <StRowText color={Theme.colors.pink}>삭제</StRowText>
+                    <BinIcon />
+                  </StRowDiv>
+                </StModifyButtonWrapper>
+              )}
+            </StKebobIconWrapper>
+          )}
+        </div>
         <StWriterInfo>
           <div>{createdAt}</div>
           <div>|</div>
--- src/pages/DetailPage/views/Content/ContentViewStyle.tsx
+++ src/pages/DetailPage/views/Content/ContentViewStyle.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
-import { Theme } from '../../../../styles';
+import { Theme, media } from '../../../../styles';
+import { KebobIcon, PenceilIcon } from '../../../../assets';
 
 export const StWrapper = styled.div`
   display: flex;
@@ -10,6 +11,11 @@ export const StWrapper = styled.div`
   border-bottom: 1px solid ${Theme.colors.gray1};
 
   margin-bottom: 36px;
+  border-radius: 20px;
+
+  ${media.mobile`
+      border-radius: 0px;
+  `}
 `;
 
 export const StTitle = styled.div`
@@ -18,6 +24,11 @@ export const StTitle = styled.div`
   color: ${Theme.colors.black};
   font-size: ${Theme.fontSizes.h1};
   font-weight: ${Theme.fontWeights.h1};
+  ${media.mobile`
+  flex:1;
+  font-size: ${Theme.fontSizes.mH2};
+  padding: 20px 0px;
+  `}
 `;
 
 export const StWriterInfo = styled.div`
@@ -27,13 +38,29 @@ export const StWriterInfo = styled.div`
   color: ${Theme.colors.gray6};
   font-size: ${Theme.fontSizes.body4};
   font-weight: ${Theme.fontWeights.body4};
-
   gap: 10px;
+
+  ${media.tablet`
+  font-size: 13px;
+  font-weight: ${Theme.fontWeights.mBody3};
+  gap: 4px;
+  align-self: flex-end;
+  `}
+
+  ${media.mobile`
+  font-size: ${Theme.fontSizes.mBody3};
+  font-weight: ${Theme.fontWeights.mBody3};
+  `}
 `;
 
 export const StTitleWrapper = styled.div`
   display: flex;
   justify-content: space-between;
+
+  ${media.mobile`
+     display: flex;
+     flex-direction : column;
+  `}
 `;
 
 export const StContentWrapper = styled.div`
@@ -49,16 +76,88 @@ export const StContentWord = styled.p`
   font-weight: ${Theme.fontWeights.body2};
   line-height: 26px;
   padding: 20px;
+
+  ${media.mobile`
+    font-size: ${Theme.fontSizes.mBody2};
+    font-weight: ${Theme.fontWeights.mBody2};
+    padding: 20px 0px;
+  `}
 `;
 
 export const StHowManyWrapper = styled.div`
   display: flex;
   width: 100%;
   justify-content: flex-end;
+
+  ${media.mobile`
+  font-size: ${Theme.fontSizes.mL1};
+  font-weight: ${Theme.fontWeights.mL1};
+  color :  ${Theme.colors.gray7};
+  `}
 `;
 
 export const StHowManyLetters = styled.div`
   color: ${Theme.colors.gray7};
   font-size: ${Theme.fontSizes.label1};
   font-weight: ${Theme.fontWeights.label1};
+
+  ${media.mobile`
+  font-size: ${Theme.fontSizes.mL1};
+  font-weight: ${Theme.fontWeights.mL1};
+  color :  ${Theme.colors.gray7};
+  `}
+`;
+
+export const StKebobIconWrapper = styled.div`
+  position: relative;
+  cursor: pointer;
+  display: inline-block;
+`;
+
+export const StyledKebob = styled(KebobIcon)`
+  width: 30px;
+  height: auto;
+`;
+
+export const StModifyButtonWrapper = styled.div`
+  position: absolute;
+  right: 0;
+  margin-top: 8px;
+  display: flex;
+  flex-direction: column;
+  width: 80px;
+  height: 94px;
+  padding: 12px;
+  box-shadow: 0px 2px 5px 0px rgba(41, 47, 61, 0.3);
+  border-radius: 8px;
+  background-color: ${Theme.colors.white};
+  overflow: hidden;
+`;
+
+export const StyledPencilIcon = styled(PenceilIcon)`
+  width: 18px;
+  height: auto;
+`;
+
+export const StRowDiv = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 3px;
+  padding-bottom: 10px;
+
+  &:last-child {
+    padding-top: 10px;
+    border-top: 1px solid ${Theme.colors.gray2};
+  }
+`;
+
+export const StRowText = styled.p<{ color: string }>`
+  display: inline-flex;
+  padding: 4px;
+  justify-content: center;
+  align-items: center;
+
+  color: ${({ color }) => color};
+  font-size: ${Theme.fontSizes.mH2};
+  font-weight: ${Theme.fontWeights.mH2};
 `;
