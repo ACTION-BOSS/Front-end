aded2617d23ad97c3e7274538419ad29d5803d34
Haru-Im <chalk323@naver.com>
2023-08-29 17:46:08 +0900

    feat: add pop up modal for resolved posts

--- src/pages/DetailPage/DetailPage.tsx
+++ src/pages/DetailPage/DetailPage.tsx
@@ -1,12 +1,44 @@
-import { FC } from 'react';
-import { DetailContainer } from './container';
+import { FC, useEffect } from 'react';
+import { DetailContainer, useDetailData } from './container';
 import { DetailLayout } from './layout';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useRecoilState } from 'recoil';
+import { useModal, EModalType } from '../../providers';
+import { $isDoneAlertedFamily } from './state';
+import { useParams } from 'react-router-dom';
 
 type DetailPageProps = {};
 
 export const DetailPage: FC<DetailPageProps> = ({}) => {
+  const { postId } = useParams();
+  const [isDoneAlerted, setisDoneAlerted] = useRecoilState(
+    $isDoneAlertedFamily(postId),
+  );
+  const { openModal, closeModal } = useModal();
+  const { postDone } = useDetailData();
+
+  console.log(postDone);
+
+  useEffect(() => {
+    console.log('as');
+    if (postDone && !isDoneAlerted) {
+      console.log('asdfasdf');
+      setisDoneAlerted(true);
+      openModal(EModalType.POP_UP, {
+        title: '해결 완료 처리된 게시물입니다',
+        cancelButton: false,
+        functionButton: {
+          label: '닫기',
+          onClick: () => {
+            closeModal();
+          },
+          theme: 'emptyBlue',
+        },
+      });
+    }
+  }, [postDone]);
+
   return (
     <DetailLayout>
       <ToastContainer />
