f83194fb7ce1eaac25db3ee5c43b138d1bec0d6a
junho01052 <junho010522@gmail.com>
2023-08-30 00:29:31 +0900

    design: modify styles in landing, create, edit page

--- src/pages/EditPost/views/EditMapView/style.tsx
+++ src/pages/EditPost/views/EditMapView/style.tsx
@@ -43,7 +43,7 @@ export const StMapText = styled.div`
 
 export const StMapContainer = styled.div`
   width: 100%;
-  height: 400px;
+  height: 410px;
   border-radius: 12px;
   position: relative;
 
