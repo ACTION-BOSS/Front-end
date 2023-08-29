430464fa998388b861bd0af7462c64f3a8c7334a
Haru-Im <chalk323@naver.com>
2023-08-29 17:43:42 +0900

    design: make detail page layout responsive

--- src/pages/DetailPage/container/DetailContainer.tsx
+++ src/pages/DetailPage/container/DetailContainer.tsx
@@ -6,6 +6,8 @@ import {
   CommentsView,
 } from '../views';
 import { useComments, useDetailData, useMetoo } from './hooks';
+import styled from 'styled-components';
+import { media } from '../../../styles';
 
 type DetailContainerProps = {};
 
@@ -45,29 +47,52 @@ export const DetailContainer: FC<DetailContainerProps> = ({}) => {
   return (
     <>
       <PhotosView imageUrlList={imageUrlList} postDone={postDone} />
-      <ContentView
-        title={title}
-        content={content}
-        createdAt={createdAt}
-        nickname={nickname}
-      />
-      <UnderButtonsView
-        address={address}
-        owner={owner}
-        handleClickMetooButton={handleClickMetooButton}
-        localMetoo={localMetoo}
-        localMetooCount={localMetooCount}
-        postDone={postDone}
-      />
-      <CommentsView
-        comments={comments}
-        postId={postId}
-        nickname={nickname}
-        onChangeComment={onChangeComment}
-        handleCreateComment={handleCreateComment}
-        handleDeleteComment={handleDeleteComment}
-        body={body}
-      />
+      <MobileContainerWrapper>
+        <MobileContentWrapper>
+          <ContentView
+            title={title}
+            content={content}
+            createdAt={createdAt}
+            nickname={nickname}
+            postId={postId}
+            owner={owner}
+          />
+          <UnderButtonsView
+            address={address}
+            owner={owner}
+            handleClickMetooButton={handleClickMetooButton}
+            localMetoo={localMetoo}
+            localMetooCount={localMetooCount}
+            postDone={postDone}
+          />
+          <CommentsView
+            comments={comments}
+            postId={postId}
+            nickname={nickname}
+            onChangeComment={onChangeComment}
+            handleCreateComment={handleCreateComment}
+            handleDeleteComment={handleDeleteComment}
+            body={body}
+            postDone={postDone}
+          />
+        </MobileContentWrapper>
+      </MobileContainerWrapper>
     </>
   );
 };
+
+const MobileContentWrapper = styled.div`
+  ${media.mobile`
+  padding : 24px;
+  background-color: white;
+  box-shadow: 0px 0px 6px 0px rgba(41, 47, 61, 0.3);
+  border-radius : 12px;
+  `}
+`;
+
+const MobileContainerWrapper = styled.div`
+  ${media.mobile`
+  padding-left:24px;
+  padding-right:24px;
+  `}
+`;
