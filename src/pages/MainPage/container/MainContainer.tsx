e3ba54960e46fe33341309a4489f117aef69c4c7
HyeonE <mag30102000@gmail.com>
2023-09-03 03:39:03 +0900

    fix: resolve issue where pins were not displayed when location search failed

--- src/pages/MainPage/container/MainContainer.tsx
+++ src/pages/MainPage/container/MainContainer.tsx
@@ -5,10 +5,10 @@ import * as s from './MainContainerStyle';
 export const MainContainer = () => {
   const [isDone, setIsDone] = useState(false);
   const [mapCoordinates, setMapCoordinates] = useState({
-    northlat: 0,
-    eastlon: 0,
-    southlat: 0,
-    westlon: 0,
+    eastlon: 126.99508722680098,
+    northlat: 37.57143011778435,
+    southlat: 37.56027203893137,
+    westlon: 126.96984867355701,
   });
 
   const mapCoordinatesChangeHandler = (
