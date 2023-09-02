f7b7b39f99a69cd01552f899d40477784dce07f4
Haru-Im <chalk323@naver.com>
2023-09-02 11:26:39 +0900

    feat: add data fetch hook in my-page

---
+++ src/pages/MyPage/container/hooks/getUserData.ts
@@ -0,0 +1,35 @@
+import { useFormContext } from 'react-hook-form';
+import { MyPageFormData } from './MyPageForm';
+import { useEffect } from 'react';
+import { useMyPageFormController } from './MyPageFormController';
+
+export const useGetUserData = () => {
+  const { setValue } = useFormContext<MyPageFormData>();
+  const { nicknameValue, onChangeNickname } = useMyPageFormController();
+
+  const originalEmail = 'asdasd@naver.com';
+  const originalPassword = 'asdf1234';
+  const originalNickname = 'haru';
+
+  console.log('hello');
+
+  useEffect(() => {
+    if (originalNickname) {
+      setValue('nickname', originalNickname);
+    }
+
+    if (originalEmail !== null) {
+      const [id, domain] = originalEmail.split('@');
+      setValue('emailId', id);
+      setValue('emailDomain', domain);
+    }
+  }, [originalEmail, originalNickname, setValue]);
+
+  return {
+    originalEmail,
+    originalPassword,
+    originalNickname,
+    nicknameValue,
+    onChangeNickname,
+  };
+};
