d5e7a329f76e2b31c4640a888a87ecff777860a0
Haru-Im <chalk323@naver.com>
2023-09-02 21:20:23 +0900

    feat: update login logic with refresh token implementation

--- src/modals/index.ts
+++ src/modals/index.ts
@@ -2,3 +2,4 @@ export * from './LoginModal/LoginModal';
 export * from './SignUpModal/SignUpModal';
 export * from './SignUpSuccessModal/index';
 export * from './PopUpModal/PopUpModal';
+export * from './SignUpModal';
